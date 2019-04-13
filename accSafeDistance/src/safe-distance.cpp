d3b6ba85eee9b384139f133b537162baaa648760
niguelchaos <niguelchaos@gmail.com>
2019-04-13 17:12:27 +0200

    began work on keeping safe distance from car in front

---
+++ accSafeDistance/src/safe-distance.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2019  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cluon-complete.hpp"
+#include "opendlv-standard-message-set.hpp"
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <mutex>
+
+using namespace std;
+using namespace cv;
+
+int32_t main(int32_t argc, char **argv) {
+    int32_t retCode{1};
+    auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+    if ( (0 == commandlineArguments.count("cid")) ||
+         (0 == commandlineArguments.count("name")) ||
+         (0 == commandlineArguments.count("width")) ||
+         (0 == commandlineArguments.count("height")) ) {
+        std::cerr << argv[0] << " attaches to a shared memory area containing an ARGB image." << std::endl;
+        std::cerr << "Usage:   " << argv[0] << " --cid=<OD4 session> --name=<name of shared memory area> [--verbose]" << std::endl;
+        std::cerr << "         --cid:    CID of the OD4Session to send and receive messages" << std::endl;
+        std::cerr << "         --name:   name of the shared memory area to attach" << std::endl;
+        std::cerr << "         --width:  width of the frame" << std::endl;
+        std::cerr << "         --height: height of the frame" << std::endl;
+        std::cerr << "Example: " << argv[0] << " --cid=112 --name=img.i420 --width=640 --height=480" << std::endl;
+    }
+    else {
+        const std::string NAME{commandlineArguments["name"]};
+        const uint32_t WIDTH{static_cast<uint32_t>(std::stoi(commandlineArguments["width"]))};
+        const uint32_t HEIGHT{static_cast<uint32_t>(std::stoi(commandlineArguments["height"]))};
+        const bool VERBOSE{commandlineArguments.count("verbose") != 0};
+
+        // Attach to the shared memory.
+        std::unique_ptr<cluon::SharedMemory> sharedMemory{new cluon::SharedMemory{NAME}};
+        if (sharedMemory && sharedMemory->valid()) {
+            std::clog << argv[0] << ": Attached to shared memory '" << sharedMemory->name() << " (" << sharedMemory->size() << " bytes)." << std::endl;
+
+            // Interface to a running OpenDaVINCI session; here, you can send and receive messages.
+            cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
+
+            // Endless loop; end the program by pressing Ctrl-C.
+         while (od4.isRunning()) {
+             cv::Mat frame;
+             cv::Mat gray;
+             cv::Mat bw;
+             cv::Mat dst;
+             std::vector < std::vector > contours;
+             std::vector approx;
+             // Wait for a notification of a new frame.
+             sharedMemory->wait();
+
+             // Lock the shared memory.
+             sharedMemory->lock();
+             {
+                 // Copy image into cvMat structure.
+                 // Be aware of that any code between lock/unlock is blocking
+                 // the camera to provide the next frame. Thus, any
+                 // computationally heavy algorithms should be placed outside
+                 // lock/unlock.
+                 cv::Mat wrapped(HEIGHT, WIDTH, CV_8UC4, sharedMemory->data());
+                 img = wrapped.clone();
+             }
+             sharedMemory->unlock();
+
+             // TODO: Do something with the frame.
+
+             // Example: Draw a red rectangle and display image.
+             cv::rectangle(img, cv::Point(50, 50), cv::Point(100, 100), cv::Scalar(0,0,255));
+
+             // Display image.
+            if (VERBOSE) {
+                 cv::imshow(sharedMemory->name().c_str(), img);
+                 cv::waitKey(1);
+              }
+         }
+      }
+     retCode = 0;
+   }
+   return retCode;
+}
+
+/**
+ * Helper function to find a cosine of angle between vectors
+ * from pt0->pt1 and pt0->pt2
+ */
+static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0) {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+/**
+ * Helper function to display text in the center of a contour
+ */
+void setLabel(cv::Mat& im, const std::string label, std::vector& contour) {
+    int fontface = cv::FONT_HERSHEY_SIMPLEX;
+    double scale = 0.4;
+    int thickness = 1;
+    int baseline = 0;
+
+    cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
+    cv::Rect r = cv::boundingRect(contour);
+
+    cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+    cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
+    cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+}
