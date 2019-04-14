ecab8e0624a31f9f823503993fd1674de90df59e
@elsada <elsada@student.chalmers.se>
2019-04-14 23:18:47 +0200

    Updated file names

--- MoveCar/src/Follow.cpp
+++ MoveCar/src/Follow.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Yue Kang
+ * Copyright (C) 2019 Elsada Lagumdzic
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
