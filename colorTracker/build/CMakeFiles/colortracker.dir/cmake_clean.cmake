ceadd6a24fc0e5fd4a8a29827ac0793e2ba9f3a9
Nuria <nuriac@student.chalmers.se>
2019-04-16 16:06:41 +0200

    build folder

---
+++ colorTracker/build/CMakeFiles/colortracker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/colortracker.dir/colortrackerexample.cpp.o"
+  "colortracker.pdb"
+  "colortracker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/colortracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
