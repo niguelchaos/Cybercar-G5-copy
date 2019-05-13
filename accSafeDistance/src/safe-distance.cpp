f4e054612dcb86431ac15cf0608e00c45d18f581
niguelchaos <niguelchaos@gmail.com>
2019-05-13 18:58:26 +0200

    seems that default brightness works better

--- accSafeDistance/src/safe-distance.cpp
+++ accSafeDistance/src/safe-distance.cpp
@@ -140,8 +140,8 @@ int32_t main(int32_t argc, char **argv) {
 
          // Pink
             int low_H_pink = 135;
-            int low_S_pink = 69;
-            int low_V_pink = 59;
+            int low_S_pink = 55;
+            int low_V_pink = 65;
             int high_H_pink = max_value_H;
             int high_S_pink = max_value;
             int high_V_pink = max_value;
@@ -150,14 +150,14 @@ int32_t main(int32_t argc, char **argv) {
             frame(Rect(Point(0, 0), Point(640, 370))).copyTo(cropped_frame);
 
       ///////////////////////// auto brightness /////////////////////
-            // Automatically increase the brightness and contrast of the video.
-            BrightnessAndContrastAuto(cropped_frame, brightened_frame, 0.6f);
-            // Convert from BGR to HSV colorspace
-            cvtColor(brightened_frame, frame_HSV, COLOR_RGB2HSV);
+            // // Automatically increase the brightness and contrast of the video.
+            // BrightnessAndContrastAuto(cropped_frame, brightened_frame, 0.6f);
+            // // Convert from BGR to HSV colorspace
+            // cvtColor(brightened_frame, frame_HSV, COLOR_RGB2HSV);
             //==//////////////////////////////////////////////////////==//
 
             ////////////// no auto brightness ////////////////////
-            // cvtColor(cropped_frame, frame_HSV, COLOR_RGB2HSV);
+            cvtColor(cropped_frame, frame_HSV, COLOR_RGB2HSV);
             ////////////////////////////////////////////////
 
             // Detect the object based on HSV Range Values
@@ -472,22 +472,22 @@ void BrightnessAndContrastAuto(const cv::Mat &src, cv::Mat &dst, float clipHistP
         clipHistPercent *= (max / 100.0f); //make percent as absolute
         // cout << "clipHistPercent % : " << clipHistPercent << endl;
 
-        clipHistPercent /= 2.1f; // left and right wings
+        clipHistPercent /= 1.4f; // left and right wings
         cout << "clipHistPercent L/R wings : " << clipHistPercent << endl;
 
-        accumulator[0] = 0;
+        // accumulator[0] = 0;
 
 // as the picamera seems to have a lower...color range than the algorithm,
 // a hardcoded little boost of brightness and contrast is given
 // by artificially bumping/lowering the values of both the upper and lower ends of the histogram
-        for (int a = 1; a < histSize/10; a++) {
-            accumulator[a] = (accumulator[a - 1] + hist.at<float>(a)) / 2;
-            // cout << "accumulator ["<< a << "]: " << accumulator[a] << endl;
-        }
-        for (int i = histSize - 1; i > histSize - histSize/11 ; i--) {
-            accumulator[i] = (accumulator[i - 1] + hist.at<float>(i)) * 2;
-            // cout << "accumulator ["<< i << "]: " << accumulator[i] << endl;
-        }
+        // for (int a = 1; a < histSize/10; a++) {
+        //     accumulator[a] = (accumulator[a - 1] + hist.at<float>(a)) / 2;
+        //     // cout << "accumulator ["<< a << "]: " << accumulator[a] << endl;
+        // }
+        // for (int i = histSize - 1; i > histSize - histSize/11 ; i--) {
+        //     accumulator[i] = (accumulator[i - 1] + hist.at<float>(i)) * 2;
+        //     // cout << "accumulator ["<< i << "]: " << accumulator[i] << endl;
+        // }
 
         // locate left cut
         minGray = 0;
